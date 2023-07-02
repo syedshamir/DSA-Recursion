ae26220adfa1f963392310150812046d13492615
Syed Shamir Raza <58033291+syedshamir@users.noreply.github.com>
2023-07-02 16:59:09 +0300

    Update recursion1.py
    added time complexity and url

--- recursion1.py
+++ recursion1.py
@@ -5,3 +5,6 @@ def TestRecursion(n):
 
 if __name__ == "__main__":
     TestRecursion(3)
+
+#time complexity is n +1 i.e 3 + 1 = 4
+# https://www.geeksforgeeks.org/how-to-analyse-loops-for-complexity-analysis-of-algorithms/
